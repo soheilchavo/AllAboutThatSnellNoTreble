7a253bcf4df77cd5e4ce7628bb7e745d616a9d4b
TheDynamicVector <63025401+soheilchavo@users.noreply.github.com>
2026-01-02 13:52:09 -0800

    elegant formula

--- scripts/portal_m.gd
+++ scripts/portal_m.gd
@@ -19,9 +19,9 @@ func _on_input_event(_viewport, event, _shape_idx):
 func _process(_delta):
 	if dragging:
 		global_position = get_global_mouse_position()
-		if Input.is_action_just_pressed("Rotate"):			
+		if Input.is_action_just_pressed("Rotate"):            
 			rotation += deg_to_rad(90)
-			out_rot = fmod(out_rot + PI/2, 2*PI)
+			out_rot += deg_to_rad(90)
 
 func teleport(body):
 	if not busy:
@@ -29,19 +29,11 @@ func teleport(body):
 			busy = true
 			var laser_direction = Vector2.from_angle(body.rotation).normalized()
 			var new_dir = Vector2(laser_direction.y, laser_direction.x)
+			print(new_dir, out_rot)
+			print(out_rot == PI, new_dir == Vector2(1, -0))
 			#Laser is 0 90 180 or 270
 			if abs(int(new_dir.y) - int(new_dir.x)) == 1:
-				if new_dir.is_equal_approx(Vector2(0, 1)) and out_rot == PI/2:
-					body.set_meta("Direction", new_dir.rotated(-PI/2))
-				elif new_dir.is_equal_approx(Vector2(-1, 0)) and out_rot == 0:
-					body.set_meta("Direction", new_dir.rotated(-PI/2))
-				elif new_dir.is_equal_approx(Vector2(0, -1)) and out_rot == 3*PI/2:
-					body.set_meta("Direction", new_dir.rotated(-PI/2))
-				elif new_dir.is_equal_approx(Vector2(1, 0)) and out_rot == PI:
-					body.set_meta("Direction", new_dir.rotated(-PI/2))
-				else:
-					body.set_meta("Direction", new_dir.rotated(PI/2))
-				
+				body.set_meta("Direction", new_dir.rotated(((2 * (int(rad_to_deg(out_rot)) % 180))/90 - 1) * (1 - 2*int(new_dir.y))*PI/2))
 				body.position = position
 			else:
 				new_dir = new_dir.rotated(sister_portal.out_rot)
